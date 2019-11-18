a83481133648e89202e960d04c659b7ed75ed7f4
Homagni Saha <homagnisaha@gmail.com>
2019-11-17 18:13:24 -0600

    Add files via upload

--- TurtleRL/turtlebot_env/patch_test.py
+++ TurtleRL/turtlebot_env/patch_test.py
@@ -1,17 +1,21 @@
-from basic_slam import slam
+#from basic_slam import slam
+from basic_slam2 import slam
 import numpy as np
 import cv2
 import copy
 
 s = slam()
 a = cv2.imread('ego_map.jpg',0)
-b = copy.copy(a)
+b = np.zeros_like(a)
 
-a = cv2.imread('persistent_map.jpg',0)
-b = cv2.imread('ego_map.jpg',0)
+#a = cv2.imread('persistent_map.jpg',0)
+#b = cv2.imread('ego_map.jpg',0)
 #a = np.ones((5,5))
 #b = np.ones((5,5))
 
-c = s.patch(copy.copy(b),b,[0,0],90)
-print("combined ",c)
-cv2.imwrite('patch_test.jpg', c)
+#Due to a faulty transformation sequence the function is written in
+# [y,x,r] needs to be passed in two stages
+c = s.patch(b,a,[0,0],-45, testing = True)
+d = s.patch(a,c,[0,0],0, testing = True)
+print("combined ",d)
+cv2.imwrite('patch_test.jpg', d)
